69f38418356db19b6236d892bcf019d9ae43a0b5
MdIshtiaque <ishtiaqueferdous109@gmail.com>
2023-02-13 13:04:01 +0600

    Create Project and Setup Dashboard backend template

---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('backend.layouts.master')
+
+@section('title')
+    dashboard
+@endsection
+
+@section('admin_content')
+    <div class="row">
+        Start From Here
+    </div>
+@endsection
